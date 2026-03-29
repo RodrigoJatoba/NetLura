7e4c7abfce0b9fe639984259bf14ae8759e326d2
Rodrigo Jatobá <rodrigosouza_al@hotmail.com>
2026-03-29 20:35:51 -0300

    Add files via upload

---
+++ catalogo-netflix/js/data.js
@@ -0,0 +1,41 @@
+export const categories = [
+    {
+        title: "Mais Assistidos",
+        items: [
+            {
+                img: "https://lionsgate.brightspotcdn.com/6e/59/31f4f526460abae715c5269d01d3/ballerina-movies-he-promo-doublehome-01.jpg", top10: true, badge: "Clássico", badgeColor: "red", progress: 10,
+                youtube: "https://www.youtube.com/watch?v=AEClFKTwRDo"
+            },
+            {
+                img: "https://rollingstone.com.br/wp-content/uploads/gladiador_2_com_paul_mescal_e_pedro_pascal_estreia_nos_cinemas_brasileiros.jpg", progress: 95,
+                youtube: "https://www.youtube.com/watch?v=thfURRSQzdw"
+            },
+            {
+                img: "https://i.ytimg.com/vi/OQgySPQ5M3Y/maxresdefault.jpg", progress: 95,
+                youtube: "https://www.youtube.com/watch?v=zckJCxYxn1g"
+            },
+            {
+                img: "https://ingresso-a.akamaihd.net/img/cinema/cartaz/14413-destaque.jpg", progress: 90,
+                youtube: "https://www.youtube.com/watch?v=a06zxOyQrAs"
+            },
+        ]
+    },
+    {
+        title: "Para maratonar",
+        items: [
+            { img: "https://m.media-amazon.com/images/M/MV5BMThlOWE3MWEtZjM4Ny00M2FiLTkyMmYtZGY3ZTcyMzM5YmNlXkEyXkFqcGdeQWpnYW1i._V1_.jpg", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=oVzVdvGIC7U" },
+            { img: "https://miro.medium.com/v2/1*JsJJGSQMeneBLtlQKOJG_A.jpeg", top10: true, youtube: "https://www.youtube.com/watch?v=VWBDosdLhoY" },
+            { img: "https://media.hugogloss.uol.com.br/uploads/2026/01/stranger-things-768x432.jpg", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=Wb3kaP580kA" },
+            { img: "https://www.meioemensagem.com.br/wp-content/uploads/2024/04/netflix.jpg", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=IiL-6sb4bs4" },
+        ]
+    },
+    {
+        title: "Séries",
+        items: [
+            { img: "https://tm.ibxk.com.br/2026/01/21/Agente_Noturno_Trailer_Data_Temporada_3_Netflix_f1ab49fae3.jpg", top10: true, youtube: "https://www.youtube.com/watch?v=cDpNFQWeqpo" },
+            { img: "https://occ-0-8407-2218.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABWY9j6L8P9xcQWTphWly95x4RqSqur7bQsW0-lIj2FCrblqLAvLLxZg5PueJG_fvODvWVpG2QUEvL6jYG5t4-9tbmXiv_AlYzoMq.jpg?r=d5c", top10: true, badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=WXYplHFM65c" },
+            { img: "https://midias.agazeta.com.br/2023/06/13/serie-coreana-caes-de-caca-da-netflix-1765643-article.jpg", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=myF-2r3h7HM" },
+            { img: "https://sm.ign.com/t/ign_br/news/a/arcane-is-/arcane-is-reportedly-the-most-expensive-animated-series-ever_ddjn.1200.jpg", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=utgH7YV4qn8" },
+        ]
+    }
+];
